e92e3c6796afb98dee0e4f6417b95b8c21846c6e
Fernando Martinez <f.martinez09@ufromail.cl>
2024-04-20 23:28:46 -0400

    add github ci

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,28 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:thread_rift/config/config.dart';
 
+// Firebase imports
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Initialize the environment
   await Environment.initEnvironment();
+
+  // Initialize Supabase
   await Supabase.initialize(
     url: Environment.supabaseUrl,
     anonKey: Environment.supabaseAnonKey,
   );
+
   runApp(const ProviderScope(child: MainApp()));
-  final supabase = Supabase.instance.client;
 }
 
 class MainApp extends ConsumerWidget {
